b074719f0e33dd7e3cec546e80414c55c70eb199
Alwyn van Wyk <alwyn.vanwyk@ixo.world>
2022-08-10 13:24:43 +0200

    first draft

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,7 +1,15 @@
 import { defineUserConfig } from 'vuepress'
 
+const { defaultTheme } = require('@vuepress/theme-default')
+
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Hello VuePress - the test',
   description: 'Just playing around',
 })
+
+module.exports = {
+  theme: defaultTheme({
+    // set config here
+  }),
+}
